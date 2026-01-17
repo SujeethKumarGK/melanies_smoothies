a597d5fd88b079538d680828ae6cf933ce60c1e3
SujeethKumarGK <85108714+SujeethKumarGK@users.noreply.github.com>
2026-01-17 23:44:08 +0530

    Update streamlit_app.py
    added the nutrition block to the if condition

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 # from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
@@ -39,6 +40,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+      
 
     st.write(ingredients_string)
 
@@ -55,10 +59,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!' +' ' + name_on_order + ':cow:' , icon="✅")
 
 #New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
 
 
 
